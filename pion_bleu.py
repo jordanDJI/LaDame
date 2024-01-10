c0f69761a6391e1d003acbfae0ad26d0be670936
MBOUNG <118815915+MBOUNG@users.noreply.github.com>
2024-01-10 15:24:38 +0100

    ajout de pion bleu

--- pion_bleu.py
+++ pion_bleu.py
@@ -9,3 +9,30 @@ class Pion_bleu(py.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x =16
         self.rect.y= 8
+
+     
+    def deplacer_pion_bleu(self, pion, direction):
+        if direction == "avant":
+            pion.rect.y -= pion.velocity
+        elif direction == "diagonale_gauche":
+            pion.rect.x -= pion.velocity
+            pion.rect.y -= pion.velocity
+        elif direction == "diagonale_droite":
+            pion.rect.x += pion.velocity
+            pion.rect.y -= pion.velocity
+
+    def promouvoir_en_dame(self, pion):
+        # Vérifier si le pion atteint la rangée la plus éloignée
+        if pion.rect.y <= 0:
+            # Remplacer le pion par une dame
+            nouvelle_dame = Dame(pion.rect.x, pion.rect.y)
+            self.pions_dames.add(nouvelle_dame)
+            self.pions_bleus.remove(pion)
+
+
+    class Dame(Pion_bleu):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = py.image.load('asset/dame_bleue.png')   
+
+    py.quit()

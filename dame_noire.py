667f3456f2b72be47e37615c68769a614979ee55
MBOUNG <118815915+MBOUNG@users.noreply.github.com>
2024-01-28 19:47:07 +0100

    ajout de la dame noire et de la dame bleue

---
+++ dame_noire.py
@@ -0,0 +1,9 @@
+import pygame as pygame
+
+class Dame(py.sprite.Sprite):
+        def__init__(self,x,y):
+                super().init__()
+                self.image=py.image.load('asset/dame_noire.jpg')
+                self.rest = self.image.get_rect()
+                self.rect.x = x
+        self.rect.y = y
--- pion_noir.py
+++ pion_noir.py
@@ -14,29 +14,25 @@ class Pion_noir(py.sprite.Sprite):
 
     
     
+    def deplacer_pion_noir(self, pion, direction):
+        if direction == "avant":
+            pion.rect.y -= pion.velocity
+        elif direction == "diagonale_gauche":
+            pion.rect.x -= pion.velocity
+            pion.rect.y -= pion.velocity
+        elif direction == "diagonale_droite":
+            pion.rect.x += pion.velocity
+            pion.rect.y -= pion.velocity
 
-    """class Pion_noir1:
-        list_pion_noir1 = []  # Liste pour stocker les pions noirs
+    
 
-        def __init__(self):
-            self.rect = py.rect()  # Assurez-vous que Rect est importé ou défini
+    def promouvoir_en_dame(self, pion):
+        # Vérifier si le pion atteint la rangée la plus éloignée
+        if pion.rect.y <= 0:
+            # Remplacer le pion par une dame
+            nouvelle_dame = Dame_noire(pion.rect.x, pion.rect.y)
+            self.pions_dames.add(nouvelle_dame)
+            self.pions_bleus.remove(pion)
 
-        def cree_les_pions1(self, pion_n):
-            pion_n = Pion_noir()
-            for i in range(pion_n.nbre_max_jeton - 1):
-                pion_n.numero = i
-                for j in range(3):  # Utilisation de 3 au lieu de 2 pour correspondre aux indices de 0 à 2
-                    if j == 0:
-                        pion_n.rect.y = 545
-                    elif j == 1:
-                        pion_n.rect.y = 468
-                    elif j == 2:
-                        pion_n.rect.y = 391
 
-                    pion_n.rect.x = 110
-                    for k in range(3):
-                        if pion_n.rect.x <= 440:
-                            pion_n.rect.x += 110
-                        elif k == 2:  # Utilisation de 2 au lieu de 3 pour correspondre aux indices de 0 à 2
-                            pion_n.rect.x = 410
-                            Pion_noir.list_pion_noir.append(pion_n)"""
+    

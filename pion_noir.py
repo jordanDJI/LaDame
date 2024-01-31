fab23472cb91632a8d91118e9e2332531847ff97
oscar Heumou <heumouo@gmail.com>
2024-01-31 22:05:05 +0100

    modification de la classe Partie et ajout de nouvelle fonction à la classe Pion_noir

--- pion_noir.py
+++ pion_noir.py
@@ -1,4 +1,6 @@
 import pygame as py
+from position_initiale import Position
+from pion_bleu import Pion_bleu
 class Pion_noir(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -11,12 +13,53 @@ class Pion_noir(py.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0 
-        self.rows = rows
-        self.col = col
+        self.position = Position(rows, col)
+        self.pions_mangeables = []
+    def position_diagonal_bas (self):
+        return [Position(self.rows + 1, self.col -1), 
+        Position(self.rows + 1, self.col + 1)]
     
-    def deplacer_pion_noir(self, pion, direction):
+    def position_diagonal_haut(sefl):
+        return [Position(sefl.rows - 1, sefl.col - 1), 
+        Position(sefl.rows - 1, sefl.col + 1)]
+    #Les positions auxquels le pion peut se déplacer dans tous les sens 
+    def Quatre_position_diagonal (self):
+        return[Position(self.rows - 1, self.col - 1),
+        Position(self.rows - 1, self.col + 1),
+        Position(self.rows + 1, self.col - 1),
+        Position(self.rows + 1, self.col + 1)]
+    
+    # Le pion peut se déplacer en diagonale lorsqu'il 
+    # mange un autre pion d'un couleur différente
+    def Quatre_position_sauter (self):
+        return[Position(self.rows - 2, self.col - 2),
+        Position(self.rows - 2, self.col + 2),
+        Position(self.rows + 2, self.col - 2),
+        Position(self.rows + 2, self.col + 2)]
+    
+    def peut_manger(self, grille):
+        positions_saut = self.Quatre_position_sauter()
+        for position_saut in positions_saut:
+            self.position.rows, self.position.col = position_saut.rows,position_saut.col
+            if 0<= self.position.rows <len (grille) and 0<= self.position.col < len(grille[0]):
+                piece_a_manger = grille[self.position.rows][self.position.col]
+                if isinstance(piece_a_manger, Pion_bleu):
+                    self.pions_mangeables.append(piece_a_manger)
+        return self.pions_mangeables
+
+
+
+    # Cette méthode spéciale compare deux instances d'une classe
+    # ayant la même valeur et renvoie la valeur booléenne.
+    def __eq__(sefl,other):
+        return sefl.position == other.position
+                    
+    def __repr__(self):
+        return "({},{})".format(self.position)
+    
+    def deplacer_pion_noir(self, direction):
         if direction == "avant":
-            pion.rect.y -= pion.velocity
+            self.position.rows -= pion.velocity,
         elif direction == "diagonale_gauche":
             pion.rect.x -= pion.velocity
             pion.rect.y -= pion.velocity
--- position_initiale.py
+++ position_initiale.py
@@ -1,8 +1,7 @@
 class Position:
     def __init__(self, rows, col):
-        
         self.rows = rows
         self.col = col
-       
+
  
         
